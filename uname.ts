c6113f189334bb232fbdb710f5747280d431a6c1
hakier <hakier29a@gmail.com>
2021-12-19 15:48:12 +0100

    feat: request permission to run uname command at runtime

--- uname.ts
+++ uname.ts
@@ -1,3 +1,11 @@
+const permission = {name: 'run', path: 'uname'};
+const permissionStatus = await Deno.permissions.request(permission);
+
+if (permissionStatus.state !== 'granted') {
+  console.log('To use this script you must grant a permission to call uname command');
+  Deno.exit(1);
+}
+
 const process = Deno.run({cmd: ['uname', '-a'], stdout: 'piped', stderr: 'piped'});
 
 console.log({status: await process.status(), output: new TextDecoder().decode(await process.output()) });
