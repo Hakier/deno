0ad88bf833c88b21d299bc027d086dff7726f661
hakier <hakier29a@gmail.com>
2021-12-20 15:01:09 +0100

    feat: allow to change a port or hostname

--- http-server.ts
+++ http-server.ts
@@ -4,5 +4,9 @@ function handler(req: Request): Response {
   return new Response("Hello, world!");
 }
 
-serve(handler);
-
+const options: Deno.ListenOptions = {
+  hostname: Deno.args[1] || '127.0.0.1',
+  port: +(Deno.args[0] || 8000),
+};
+serve(handler, options);
+console.info(`Server started: ${options.hostname}:${options.port}`)
